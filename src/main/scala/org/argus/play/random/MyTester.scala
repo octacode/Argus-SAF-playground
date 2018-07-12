852d8dd172a940133c85033906956c50d1d29454
octacode <allblue.shasha@gmail.com>
2018-07-12 15:06:28 -0400

    used argus_playground as a base

--- src/main/scala/org/argus/play/random/MyTester.scala
+++ src/main/scala/org/argus/play/random/MyTester.scala
@@ -1,7 +1,10 @@
 package org.argus.play.random
 
+import java.io.PrintWriter
+
 import org.argus.amandroid.alir.componentSummary.ApkYard
 import org.argus.amandroid.core.decompile.{DecompileLayout, DecompileStrategy, DecompilerSettings}
+import org.argus.jawa.alir.JawaAlirInfoProvider
 import org.argus.jawa.core.DefaultReporter
 import org.argus.jawa.core.util._
 
@@ -17,11 +20,27 @@ object MyTester {
     val yard = new ApkYard(reporter)
     val layout = DecompileLayout(outputUri)
     val strategy = DecompileStrategy(layout)
-    val settings = DecompilerSettings(debugMode = false, forceDelete = true, strategy, reporter)
-    val apk = yard.loadApk(fileUri, settings, collectInfo = false, resolveCallBack = false)
+    val settings = DecompilerSettings(debugMode = true, forceDelete = true, strategy, reporter)
+    val apk = yard.loadApk(fileUri, settings, collectInfo = true, resolveCallBack = false)
 
-    val activities = apk.model.getActivities
     val permissions = apk.model.getUsesPermissions
-    println(activities.size + "++++++" + permissions.size)
+    val certificates = apk.model.getCertificates
+
+    val allMethods = apk.getApplicationClasses.map(c => c.getDeclaredMethods).reduce(_ ++ _)
+        allMethods.foreach { m =>
+          println(m.getSignature)
+          println(m.retrieveCode)
+          val cfg = JawaAlirInfoProvider.getCfg(m)
+          cfg.toDot(new PrintWriter(System.out))
+        }
+
+//    printMe("Permissions")(permissions.toList)
+  }
+
+  def printMe(message: String)(item: List[String]): Unit = {
+    println("\t\t"+message)
+    item.foreach {
+      hello => println(hello)
+    }
   }
 }
